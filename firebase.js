1afd6b46c6a3a9140f0f0c124df7ae5b745e36f8
Ivan <ravic.ivan88@gmail.com>
2022-10-12 10:45:12 +0200

    Testing env.file

--- firebase.js
+++ firebase.js
@@ -4,15 +4,15 @@ import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
 
-  authDomain: process.env.AUTH_DOMAIN,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
 
-  projectId: process.env.PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
 
-  storageBucket: process.env.STORAGE_BUCKET,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
 
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
 
   appId: process.env.APP_ID,
 };
